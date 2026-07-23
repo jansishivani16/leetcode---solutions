5bb4ef8483a2498082c471bccce348be9628755a
jansishivani16 <jhansishivani123@gmail.com>
2026-07-23 14:06:01 +0530

    Time: 39 ms (55.91%) | Memory: 13.4 MB (62.94%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeDuplicates(self, s):
+        st=[]
+        for ch in s:
+            if st and st[-1]==ch:
+                st.pop()
+            else:
+                st.append(ch)
+        return "".join(st)
+
+        
